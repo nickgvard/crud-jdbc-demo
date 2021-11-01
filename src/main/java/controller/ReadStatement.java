dc51340079c1da200ac08d077a187eecf368cf47
nickgvard <project.3players@gmail.com>
2021-11-01 16:40:26 +0300

    Adding controller, model, repository, service, view packages and classes

---
+++ src/main/java/controller/ReadStatement.java
@@ -0,0 +1,8 @@
+package controller;
+
+/**
+ * @author Nikita Gvardeev 01.11.2021
+ * email gvardeev@po-korf.ru
+ */
+public class ReadStatement {
+}
---
+++ src/main/java/model/entity/Label.java
@@ -0,0 +1,8 @@
+package model.entity;
+
+/**
+ * @author Nikita Gvardeev 01.11.2021
+ * email gvardeev@po-korf.ru
+ */
+public class Label {
+}
