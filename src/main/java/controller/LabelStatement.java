cdff50fd8692c3bbee314f5c007184271d0d64c1
nickgvard <project.3players@gmail.com>
2021-11-02 10:32:29 +0300

    Добавил методы в LabelStatement

--- src/main/java/controller/LabelStatement.java
+++ src/main/java/controller/LabelStatement.java
@@ -1,8 +1,32 @@
 package controller;
 
+import service.LabelService;
+
 /**
  * @author Nikita Gvardeev 01.11.2021
  * email gvardeev@po-korf.ru
  */
 public class LabelStatement {
+
+    private final LabelService labelService;
+
+    public LabelStatement() {
+        labelService = new LabelService();
+    }
+
+    public void createLabel() {
+
+    }
+
+    public void readLabel() {
+
+    }
+
+    public void updateLabel() {
+
+    }
+
+    public void deleteLabel() {
+
+    }
 }
