a1fa9f4bff14dc4b23044295e7dd04a9283c8df6
nickgvard <project.3players@gmail.com>
2021-11-02 17:00:21 +0300

    Add LabelServiceTest

---
+++ src/main/Test/service/LabelServiceTest.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Nikita Gvardeev 02.11.2021
+ * email gvardeev@po-korf.ru
+ */
+public class LabelServiceTest {
+
+    @Test
+    public void createLabelService() {
+    }
+}
